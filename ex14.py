1975fdb5368cdb6eb57b4365c19fdebb55123037
Aditi Bhandari <aditibhandari24@gmail.com>
2017-08-07 18:21:53 -0400

    more exercises

--- ex14.py
+++ ex14.py
@@ -1,18 +1,18 @@
 from sys import argv
 
-script, user_name = argv
-prompt = '> '
+script, user_name, my_name = argv
+reply = '> '
 
-print "Hi %s, I'm the %s script." % (user_name, script)
+print "Hi %s, I'm the %s script named %s." % (user_name, script, my_name)
 print "I'd like to ask you a few questions."
 print "Do you like me %s?" % user_name
-likes = raw_input(prompt)
+likes = raw_input(reply)
 
 print "Where do you live %s?" % user_name
-lives = raw_input(prompt)
+lives = raw_input(reply)
 
 print "What kind of computer do you have?"
-computer = raw_input(prompt)
+computer = raw_input(reply)
 
 print """
 Alright, so you said %r about liking me.
