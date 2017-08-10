9240266fc11fb3991b064a5956ae4103efd185c8
Aditi Bhandari <aditibhandari24@gmail.com>
2017-08-10 19:12:08 -0400

    math functions

--- ex21.py
+++ ex21.py
@@ -30,3 +30,5 @@ print "Here is a puzzle."
 what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
 
 print "That becomes: ", what, "Can you do it by hand?"
+
+new_what = divide(multiply(weight, subtract(height, age)), 2)
